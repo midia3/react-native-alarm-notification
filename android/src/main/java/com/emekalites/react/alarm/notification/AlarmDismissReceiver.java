21bf69a20d73129c775564c6e5e7e3940993f842
Henrique Tensfeld <henrique.tensfeld@midia3.com.br>
2022-12-07 18:33:46 -0500

    Fix: dismiss by swiping notification stops sound if app is not running in background

--- android/src/main/java/com/emekalites/react/alarm/notification/AlarmDismissReceiver.java
+++ android/src/main/java/com/emekalites/react/alarm/notification/AlarmDismissReceiver.java
@@ -12,18 +12,19 @@ public class AlarmDismissReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         AlarmUtil alarmUtil = new AlarmUtil((Application) context.getApplicationContext());
         try {
+            int notificationId = intent.getExtras().getInt(Constants.DISMISSED_NOTIFICATION_ID);
+
             if (ANModule.getReactAppContext() != null) {
-                int notificationId = intent.getExtras().getInt(Constants.DISMISSED_NOTIFICATION_ID);
                 ANModule.getReactAppContext().getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("OnNotificationDismissed", "{\"id\": \"" + notificationId + "\"}");
-
-                alarmUtil.removeFiredNotification(notificationId);
-
-                alarmUtil.doCancelAlarm(notificationId);
-
-                alarmUtil.stopAlarmSound();
-
-                alarmUtil.repeatAlarmById(notificationId);
             }
+
+            alarmUtil.removeFiredNotification(notificationId);
+
+            alarmUtil.doCancelAlarm(notificationId);
+
+            alarmUtil.stopAlarmSound();
+
+            alarmUtil.repeatAlarmById(notificationId);
         } catch (Exception e) {
             alarmUtil.stopAlarmSound();
             System.err.println("Exception when handling notification dismiss. " + e);
